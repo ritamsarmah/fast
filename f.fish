100f240886fce1342ba1f3e69c2f9d483f4afee1
Ritam Sarmah <sarmah.ritam@gmail.com>
2023-10-26 21:39:59 -0700

    Use binary exit status for shell wrapper

--- f.fish
+++ f.fish
@@ -7,9 +7,13 @@ function f
     # Forward arguments
     fast $argv
 
+    set -l fast_status $status
     # Check if fast exited successfully and wrote command to tmp file
-    if test $status -eq 0 && test -e "$tmp_file"
+    if test $fast_status -eq 0 && test -e "$tmp_file"
         set -l cmd (cat $tmp_file)
         eval "$cmd"
     end
+
+    # Exit with fast status
+    return $fast_status
 end
